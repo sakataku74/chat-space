087c20a744ce0b51bb24616fde1459ea5244bd94
sakaitakuma <63274132+sakaitakuma@users.noreply.github.com>
2020-04-16 22:32:48 +0900

    リンクの設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
   get 'messages/index'
   root "messages#index"
+  resources :users, only: [:edit, :update]
+  resources :groups, only: [:new, :create]
 end
