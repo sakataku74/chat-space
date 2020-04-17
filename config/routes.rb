d7c72a2dd640defbad6ea013ed0080e15ed82b32
sakaitakuma <63274132+sakaitakuma@users.noreply.github.com>
2020-04-17 15:18:12 +0900

    グループ登録

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  get 'messages/index'
-  root "messages#index"
+  devise_for :users
+  root 'groups#index'
   resources :users, only: [:edit, :update]
-  resources :groups, only: [:new, :create]
+  resources :groups, only: [:index, :new, :create, :edit, :update]
 end
